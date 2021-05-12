fcd33ba72bf156a8454d52053cebd9008d24fe3d
eligarlo <eligarlo.developer@gmail.com>
2021-05-12 20:12:26 +0300

    Add Header and Footer

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import styles from '../styles/Footer.module.css'
+
+export default function Footer() {
+  const currentYear = () => {
+    const d = new Date()
+    return d.getFullYear()
+  }
+
+  return (
+    <footer className={styles.footer}>
+      <p>Copyright &copy; DJ Events {currentYear()}</p>
+      <p>
+        <Link href='/about'>About This Project</Link>
+      </p>
+    </footer>
+  )
+}
